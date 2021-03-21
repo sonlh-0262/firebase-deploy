ff10ea6b597ec99d279e8d94362cdd14118a44ef
lanh.hung.son <SUN-ASTERISK\lanh.hung.son@b120262-lt.sun-asterisk.com>
2021-03-21 19:08:38 +0700

    add todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn about React', isCompleted: false },
+    { text: 'Meet friend for lunch', isCompleted: false },
+    { text: 'Build really cool todo app', isCompleted: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = todos.map((todo, _index) => {
+      if(_index === index) return { ...todo, isCompleted: true }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((todo, _index) => {
+      return _index !== index
+    });
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload sonlh.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        { todos.map(( todo, index ) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+
+        <TodoForm
+          addTodo={addTodo}
+        />
+      </div>
     </div>
   );
 }
